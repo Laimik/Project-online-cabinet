69ceebd86954e98b1d75a17915cb761e57d3a230
Captain Jack The Sparrow <stg40arma2@gmail.com>
2020-08-27 17:18:35 +0300

    fix bug for docker

--- backend/src/app.js
+++ backend/src/app.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use('/auth', authRouter);
 app.use('/api/v1/counter_types', counterTypeRouter);
