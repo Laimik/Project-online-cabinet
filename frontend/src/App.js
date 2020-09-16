9906cc48e791fc130b39e3737e9bd218c174c593
komalapa <komalapa@gmail.com>
2020-09-16 13:43:56 +0400

    Вкладка регистрации готова

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,11 +10,8 @@ import LoginForm from './Components/auth/loginForm/loginForm'
 
 import {Provider} from 'react-redux';
 import {initStore} from './store'
-import {signIn,signUp,logOut}from './store/authActions'
 const store = initStore();
-//store.dispatch(signUp("pika@chu.poke","pikapika","pikachu", "+7(123)456-78-90"));
-//store.dispatch(signIn("pika@chu.poke","pikapika",));
-//store.dispatch(logOut());
+
 function App() {
   return (
     <Provider store = {store}>
--- frontend/src/Components/auth/loginForm/loginForm.jsx
+++ frontend/src/Components/auth/loginForm/loginForm.jsx
@@ -12,53 +12,57 @@ import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
 import Alert from '@material-ui/lab/Alert';
 
-import {connect} from 'react-redux';
-import {bindActionCreators} from 'redux';
-import {signIn,signUp,logOut}from '../../../store/authActions'
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { signIn, signUp, logOut } from '../../../store/authActions'
 async function signInReq(email, password) {
   const response = await fetch(
-      `http://localhost:3000/auth/sign_in`, {
-          method: 'POST',
-          headers: {
-              'Accept': 'application/json',
-              'Content-Type': 'application/json',
-              
-          },
-          body: JSON.stringify({
-              email,
-              password,
-          }),
-      });
+    `http://localhost:3000/auth/sign_in`, {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
 
-      console.log(response)   
-  if (response.ok){
-      const json = await response.json();
-      //await setToken(json.accessToken);
-      console.log(json)
-      return json
-  } 
-  else if (response.status==401)   {console.log("error pass"); 
-  return null}
+    },
+    body: JSON.stringify({
+      email,
+      password,
+    }),
+  });
+
+ //console.log(response)
+  if (response.ok) {
+    const json = await response.json();
+    //await setToken(json.accessToken);
+    //console.log(json)
+    return json
+  }
+  else if (response.status == 401) {
+   // console.log("error pass");
+    return null
+  }
 }
- async function signUpReq(email, password, name, phoneNumber) {
+async function signUpReq(email, password, name, phoneNumber) {
   const response = await fetch(
-      `http://localhost:3000/auth/sign_up`, {
-          method: 'POST',
-          headers: {
-              'Accept': 'application/json',
-              'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({
-              email,
-              password,
-              name,
-              phone_number: phoneNumber
-          }),
-      });
-  console.log(response.body)
-  if (!response.ok){
-      //ToDo обработка ошибок регистрации
-      //throw new Error( await response.json);
+    `http://localhost:3000/auth/sign_up`, {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      email,
+      password,
+      name,
+      phone_number: phoneNumber
+    }),
+  });
+  if (response.ok) {
+    return "registred"
+  } else if (response.status==302){ 
+    return "dublicate"
+  } else {
+    return "error"
   }
 }
 function TabPanel(props) {
@@ -100,50 +104,58 @@ const useStyles = makeStyles((theme) => ({
     backgroundColor: theme.palette.background.paper,
     // maxWidth:"80%",
     // margin:"10% auto"
-    
+
   },
-  authForm:{
-    display:"flex",
+  authForm: {
+    display: "flex",
     flexDirection: "column",
-    alignItems:"center",
+    alignItems: "center",
     justifyContent: "center",
-    height:"80vh",
-    '& div':{
-      marginBottom:'10px'
+    height: "80vh",
+    '& div': {
+      marginBottom: '10px'
     }
   },
 
-  authTabsHeader:{
-    display:"flex",
-    alignSelf:"center",
+  authTabsHeader: {
+    display: "flex",
+    alignSelf: "center",
   },
-  badInput:{
+  badInput: {
     borderColor: "red"
   },
 
-  authAlert:{
-    position:"absolute",
-    bottom:'10px'
+  authAlert: {
+    position: "absolute",
+    bottom: '10px'
   }
 }));
 
- function LoginForm(props) {
+function LoginForm(props) {
   const classes = useStyles();
-  const [value,setValue] = React.useState(0);
+  const [value, setValue] = React.useState(0);
   const [emailValue, setEmailValue] = React.useState('');
   const [passwordValue, setPasswordValue] = React.useState('');
   const [passwordDoubleValue, setPasswordDoubleValue] = React.useState('');
-  const classBadInput='';
-  const [requiredEmpty,setRequiredEmpty]=React.useState(false);
-  const [isWrongPassword,setIsWrongPassword]=React.useState(false);
-  const [isPasswordEqual,setIsPasswordsEqual]=React.useState(true);
-  const [isWrongEmail,setIsWrongEmail]=React.useState(false);
+  const [nameValue, setNameValue] = React.useState('');
+  const [phoneValue, setPhoneValue] = React.useState('');
+  const classBadInput = '';
+  const [requiredEmpty, setRequiredEmpty] = React.useState(false);
+  const [isWrongPassword, setIsWrongPassword] = React.useState(false);
+  const [isPasswordEqual, setIsPasswordsEqual] = React.useState(true);
+  const [isWrongEmail, setIsWrongEmail] = React.useState(false);
+  const [isWrongName, setIsWrongName] = React.useState(false);
+  const [isWrongPhone, setIsWrongPhone] = React.useState(false);
+  const [isUserExist, setIsUserExist] = React.useState(false);
+  const [isRegistred, setIsRegistred] = React.useState(false);
   //console.log(props)
   const handleChange = (event, newValue) => {
     setValue(newValue);
     //console.log(newValue)
   };
-  const regExpEmail=/^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[a-zA-Z0-9]+$/;
+  const regExpEmail = /^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[a-zA-Z0-9]+$/;
+  const regExpName = /^[a-zA-Zа-яА-я\s]+$/;
+  const regExpPhone = /\+7\(\d{3}\)\d{3}-\d{2}-\d{2}/;;
   const handleChangeEmail = (event) => {
     setEmailValue(event.target.value);
     //console.log(event.target.value)
@@ -157,160 +169,211 @@ const useStyles = makeStyles((theme) => ({
 
     //console.log(event.target.value)
   };
-//   const handleChange = (event) => {
-//     this.setState({ value: event.target.value });
+  const handleChangeName = (event) => {
+    setNameValue(event.target.value);
 
-// }
- 
-const  handleEnter = async(event) =>
-  {
+    //console.log(event.target.value)
+  };
+  const handleChangePhone = (event) => {
+    setPhoneValue(event.target.value);
+
+    //console.log(event.target.value)
+  };
+  //   const handleChange = (event) => {
+  //     this.setState({ value: event.target.value });
+
+  // }
+
+  const handleEnter = async (event) => {
     event.preventDefault();
     //console.log("enter " + value + emailValue)
-    if (!emailValue || !passwordValue){
+    if (!emailValue || !passwordValue) {
       setRequiredEmpty(true)
-    } else{
-      setRequiredEmpty(false)
-      if ( !regExpEmail.test(emailValue)){setIsWrongEmail(true)
-        // console.log("email wrong")
-          
-        }else{
-        //  console.log("email ok")
-          setIsWrongEmail(false)
-          await signIn(emailValue, passwordValue);
-          // //this.setState({signedIn: true});
-           const token=await signInReq(emailValue, passwordValue);
-           if (token) {
-             props.signIn(emailValue,token);
-             setIsWrongPassword(false)
-            }else {
-             setIsWrongPassword(true)
-           }
-         
-        } 
-    }
-    
-  }
-  const handleRegistration = async(event) => {
-    event.preventDefault();
-    if (!emailValue || !passwordValue || !passwordDoubleValue){
-      setRequiredEmpty(true);
-    //  console.log("full") 
     } else {
       setRequiredEmpty(false)
-      if ( !regExpEmail.test(emailValue)){setIsWrongEmail(true)
-      // console.log("email wrong")
-      }else{
-      //  console.log("email ok")
+      if (!regExpEmail.test(emailValue)) {
+        setIsWrongEmail(true)
+        // console.log("email wrong")
+
+      } else {
+        //  console.log("email ok")
         setIsWrongEmail(false)
-        if (passwordValue!==passwordDoubleValue) {setIsPasswordsEqual(false) 
-      //    console.log("pwd no")
-        } else {setIsPasswordsEqual(true)
-      //    console.log("pwd ok")
-          await signUpReq(emailValue, passwordValue,'name', '+7(965)342-76-53');
-          // //this.setState({signedIn: true});
-           console.log("signed up")
+        await signIn(emailValue, passwordValue);
+        // //this.setState({signedIn: true});
+        const token = await signInReq(emailValue, passwordValue);
+        if (token) {
+          props.signIn(emailValue, token);
+          setIsWrongPassword(false)
+        } else {
+          setIsWrongPassword(true)
         }
-      }     
+
+      }
     }
-    
-   
+
+  }
+  const handleRegistration = async (event) => {
+    setIsUserExist(false);
+    setIsRegistred(false);
+    event.preventDefault();
+    if (!emailValue || !passwordValue || !passwordDoubleValue || !nameValue || !phoneValue) {
+      setRequiredEmpty(true);
+      //  console.log("full") 
+    } else {
+      setRequiredEmpty(false)
+      if (!regExpEmail.test(emailValue)) {
+        setIsWrongEmail(true)
+        // console.log("email wrong")
+      } else {
+        //  console.log("email ok")
+        setIsWrongEmail(false)
+        if (!regExpName.test(nameValue)) {
+          setIsWrongName(true)
+        } else {
+          setIsWrongName(false);
+          if (!regExpPhone.test(phoneValue)) {
+            setIsWrongPhone(true)
+          } else {
+            setIsWrongPhone(false);
+            if (passwordValue !== passwordDoubleValue) {
+              setIsPasswordsEqual(false)
+              //    console.log("pwd no")
+            } else {
+              setIsPasswordsEqual(true)
+              //    console.log("pwd ok")
+              const result = await signUpReq(emailValue, passwordValue, nameValue, phoneValue);
+              // //this.setState({signedIn: true});
+              //console.log("signed up")
+              if (result=="registred") {
+                setIsRegistred(true);
+                props.signUp(emailValue)
+              } else if (result == "dublicate") {
+                setIsUserExist(true);
+              }
+            }
+          }
+        }
+      }
+    }
+
+
   };
-  
+
   return (
     <div className={classes.authTabs}>
       <AppBar position="static"  >
         <Tabs value={value} onChange={handleChange} aria-label="auth and registration tabs" className={classes.authTabsHeader}>
-          <Tab label="Вход" {...a11yProps(0)}  />
+          <Tab label="Вход" {...a11yProps(0)} />
           <Tab label="Регистрация" {...a11yProps(1)} />
         </Tabs>
       </AppBar>
       <TabPanel value={value} index={0} className={classes.authTabs}>
         <form className={classes.authForm} onSubmit={handleEnter}>
-            <TextField
-            
+          <TextField
+
             id="auth-email"
             label="Электронная почта"
             variant="outlined"
             onChange={handleChangeEmail}
             value={emailValue}
             required
-            />
-            <TextField
-                id="auth-password"
-                label="Пароль"
-                type="password"
-                autoComplete="current-password"
-                variant="outlined"
-                onChange={handleChangePassword}
-                value={passwordValue}
-                className={classBadInput}
-                required
-            />
+          />
+          <TextField
+            id="auth-password"
+            label="Пароль"
+            type="password"
+            autoComplete="current-password"
+            variant="outlined"
+            onChange={handleChangePassword}
+            value={passwordValue}
+            className={classBadInput}
+            required
+          />
 
-            <FormControlLabel
-                control={
-                    <Checkbox  name="saveSession" color="primary"/>
-                }
-                label="Запомнить меня"
-            />    
-            <Button variant="contained" color="primary" onClick={handleEnter} type="submit">
-                Войти
+          <FormControlLabel
+            control={
+              <Checkbox name="saveSession" color="primary" />
+            }
+            label="Запомнить меня"
+          />
+          <Button variant="contained" color="primary" onClick={handleEnter} type="submit">
+            Войти
             </Button>
-            { requiredEmpty && <Alert className={classes.authAlert} severity="error">Необходимо заполнить все поля</Alert>}
-            { (!requiredEmpty && isWrongEmail) && <Alert className={classes.authAlert} severity="error">Email не соответствует формату</Alert>}
-            { (!requiredEmpty && !isWrongEmail &&isWrongPassword) && <Alert className={classes.authAlert} severity="error">Неверная пара логин и пароль</Alert>}
+          {requiredEmpty && <Alert className={classes.authAlert} severity="error">Необходимо заполнить все поля</Alert>}
+          {(!requiredEmpty && isWrongEmail) && <Alert className={classes.authAlert} severity="error">Email не соответствует формату</Alert>}
+          {(!requiredEmpty && !isWrongEmail && isWrongPassword) && <Alert className={classes.authAlert} severity="error">Неверная пара логин и пароль</Alert>}
 
         </form>
       </TabPanel>
       <TabPanel value={value} index={1}>
-      <form className={classes.authForm} onSubmit={handleRegistration}>
-            <TextField
-            
+        <form className={classes.authForm} onSubmit={handleRegistration}>
+          <TextField
+
             id="reg-email"
             label="Электронная почта"
             variant="outlined"
             onChange={handleChangeEmail}
             value={emailValue}
-            />
-            <TextField
-                id="reg-password"
-                label="Пароль"
-                type="password"
-                autoComplete="current-password"
-                variant="outlined"
-                className={classBadInput}
-                onChange={handleChangePassword}
-                value={passwordValue}
-            />
-            <TextField
-                id="reg-password-repeat"
-                label="Повтор пароля"
-                type="password"
-                autoComplete="current-password"
-                variant="outlined"
-                className={classBadInput}
-                onChange={handleChangePasswordDouble}
-                value={passwordDoubleValue}
-            />
-            <Button variant="contained" color="primary" type="submit">
-                Регистрация
+          />
+          <TextField
+
+            id="reg-name"
+            label="Имя"
+            variant="outlined"
+            onChange={handleChangeName}
+            value={nameValue}
+          />
+          <TextField
+
+            id="reg-phone"
+            label="Телефон"
+            variant="outlined"
+            onChange={handleChangePhone}
+            value={phoneValue}
+          />
+          <TextField
+            id="reg-password"
+            label="Пароль"
+            type="password"
+            autoComplete="current-password"
+            variant="outlined"
+            className={classBadInput}
+            onChange={handleChangePassword}
+            value={passwordValue}
+          />
+          <TextField
+            id="reg-password-repeat"
+            label="Повтор пароля"
+            type="password"
+            autoComplete="current-password"
+            variant="outlined"
+            className={classBadInput}
+            onChange={handleChangePasswordDouble}
+            value={passwordDoubleValue}
+          />
+          <Button variant="contained" color="primary" type="submit">
+            Регистрация
             </Button>
-            { requiredEmpty && <Alert className={classes.authAlert}  severity="error">Необходимо заполнить все поля</Alert>}
-            { (!requiredEmpty && isWrongEmail) && <Alert className={classes.authAlert} severity="error">Email не соответствует формату</Alert>}
-            { (!requiredEmpty && !isWrongEmail &&!isPasswordEqual) && <Alert className={classes.authAlert} severity="error">Пароли не совпадают</Alert>}
+          {requiredEmpty && <Alert className={classes.authAlert} severity="error">Необходимо заполнить все поля</Alert>}
+          {(!requiredEmpty && isWrongEmail) && <Alert className={classes.authAlert} severity="error">Email не соответствует формату</Alert>}
+          {(!requiredEmpty && !isWrongEmail && isWrongName) && <Alert className={classes.authAlert} severity="error">Недопустимое имя</Alert>}
+          {(!requiredEmpty && !isWrongEmail && !isWrongName && isWrongPhone) && <Alert className={classes.authAlert} severity="error">Телефон должен соответствовать формату:+7(ХХХ)ХХХ-ХХ-ХХ</Alert>}
+          {(!requiredEmpty && !isWrongEmail && !isWrongName && !isWrongPhone && !isPasswordEqual) && <Alert className={classes.authAlert} severity="error">Пароли не совпадают</Alert>}
+          {(isUserExist) && <Alert className={classes.authAlert} severity="error">Такой пользователь уже зарегистрирован</Alert>}
+          {(isRegistred) && <Alert className={classes.authAlert} severity="success">Новый пользователь зарегистрирован</Alert>}
         </form>
       </TabPanel>
 
     </div>
   );
 }
-const mapStateToProps=(store)=>{
+const mapStateToProps = (store) => {
   console.log(store)
   return {
     accessToken: store.accessToken,
     email: store.email
   }
 }
-const mapDispatchToProps=(dispatch)=>bindActionCreators({signIn,signUp},dispatch)
+const mapDispatchToProps = (dispatch) => bindActionCreators({ signIn, signUp }, dispatch)
 
-export default connect(mapStateToProps,mapDispatchToProps)(LoginForm)
+export default connect(mapStateToProps, mapDispatchToProps)(LoginForm)
