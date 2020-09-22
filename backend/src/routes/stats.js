d2f0c4a8bbe94e94c8f0309bfbcf9378b6715dea
stg40 <stg40stg@gmail.com>
2020-09-22 19:14:27 +0300

    add API stats

--- backend/src/routes/stats.js
+++ backend/src/routes/stats.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const authenticateJWT = require("../middlewares/authenticateJWT");
 
 const bodyParser = require("body-parser");
+const {getDynamic} = require("../services/statsService");
 const {getHistory} = require("../services/statsService");
 const urlencodedParser = bodyParser.urlencoded({extended: false});
 
@@ -14,6 +15,58 @@ router.get('/history', authenticateJWT, async (req, res) => {
         try {
             const counterValues = await getHistory(user.id, addresses, counterTypes, counters);
             res.status(200).json(counterValues);
+        } catch (e) {
+            console.error(e);
+            res.sendStatus(500);
+        }
+    }
+)
+
+router.get('/dynamic', authenticateJWT, async (req, res) => {
+        const user = req.user;
+        const addresses = req.query.addresses;
+        const counterTypes = req.query.counter_types;
+        const counters = req.query.counters;
+        try {
+            const raw = await getDynamic(user.id, addresses, counterTypes, counters);
+
+            const labels = [];
+            const series = [];
+            for (const item of raw) {
+                if (!labels.find(label => label === item.registry_time)) {
+                    console.log(labels);
+                    console.log(item.registry_time);
+                    labels.push(item.registry_time);
+                }
+
+                if (!series.includes(item.counter_type)) {
+                    series.push(item.counter_type)
+                }
+            }
+
+            let chartData = {
+                labels: labels,
+                datasets: series.map(single => { return {
+                    label: single,
+                    data: []
+                }})
+            }
+
+            for (const label of chartData.labels) {
+                for (const dataset of chartData.datasets) {
+                    const values = raw.filter(item =>
+                        item.registry_time === label &&
+                        raw.counter_type === dataset.label);
+
+                    let sum = values.reduce(function(a, b){
+                        return a.summ + b.summ;
+                    }, 0);
+
+                    dataset.data.push(sum);
+                }
+            }
+
+            res.status(200).json(chartData);
         } catch (e) {
             console.error(e);
             res.sendStatus(500);
--- backend/src/services/statsService.js
+++ backend/src/services/statsService.js
@@ -29,6 +29,7 @@ module.exports = {
         }
 
         query += where;
+        query += '\nORDER BY registry_time DESC, counter_type, counter_name'
         console.log(query);
 
         const [rows] = await pool.execute(
@@ -36,5 +37,46 @@ module.exports = {
             params,
         );
         return rows;
-    }
+    },
+
+    getDynamic: async (userId, addresses, counterTypes, counters) => {
+        const pool = await require("../database/database").connectionPool;
+        let query = "SELECT ct.name AS counter_type,\n" +
+            "       cv.registry_time,\n" +
+            "       (SELECT IFNULL(rate, 0) AS rate\n" +
+            "        FROM rate r\n" +
+            "        WHERE r.counter_type_id = ct.id AND r.date_begin <= cv.registry_time\n" +
+            "        ORDER BY r.date_begin DESC LIMIT 1) * cv.value - (SELECT IFNULL(pcv.value, 0) AS rate\n" +
+            "        FROM counter_values pcv\n" +
+            "        WHERE pcv.counter_id = cv.counter_id AND pcv.registry_time < cv.registry_time\n" +
+            "        ORDER BY pcv.registry_time DESC LIMIT 1) AS summ\n" +
+            "FROM counter_values cv\n" +
+            "INNER JOIN counters c on cv.counter_id = c.id AND c.user_id = ?\n" +
+            "INNER JOIN counter_types ct on c.counter_type_id = ct.id\n" +
+            "WHERE registry_time BETWEEN adddate(now(), INTERVAL -1 YEAR ) AND now()";
+
+        let params = [userId]
+
+        let where = '';
+        if (addresses) {
+            where += `\nAND c.address_id IN (${addresses.join(',')})`;
+        }
+
+        if (counterTypes) {
+            where += `\nAND c.counter_type_id IN (${counterTypes.join(',')})`;
+        }
+        if (counters) {
+            where += `\nAND c.id IN (${counters.join(',')})`;
+        }
+
+        query += where;
+        query += '\nORDER BY registry_time DESC, counter_type'
+        console.log(query);
+
+        const [rows] = await pool.execute(
+            query,
+            params,
+        );
+        return rows;
+    },
 }
